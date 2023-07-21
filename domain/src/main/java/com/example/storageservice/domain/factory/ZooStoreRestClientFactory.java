0efe21e28c7d8c25ce02bb566106282ecbb72703
user <danez7000@gmail.com>
2023-07-21 14:53:10 +0300

    Snapshot 0.0.2

---
+++ domain/src/main/java/com/example/storageservice/domain/factory/ZooStoreRestClientFactory.java
@@ -0,0 +1,22 @@
+package com.example.storageservice.domain.factory;
+import com.example.zoostore.restexport.restoperation.ZooStoreRestClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ZooStoreRestClientFactory {
+    @Bean
+    ZooStoreRestClient getRestExportClient() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        return Feign.builder()
+                .encoder(new JacksonEncoder(objectMapper))
+                .decoder(new JacksonDecoder(objectMapper))
+                .target(ZooStoreRestClient.class, "http://localhost:8080");
+    }
+}
--- rest/src/main/java/com/example/storageservice/rest/StorageServiceApplication.java
+++ rest/src/main/java/com/example/storageservice/rest/StorageServiceApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
@@ -16,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class StorageServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(StorageServiceApplication.class, args);
     }
 
